ed7f133b68ea8b8045e29c55a59c060968542737
AlexandrGonin <150127341+AlexandrGonin@users.noreply.github.com>
2025-04-20 19:37:02 +0500

    123

--- src/App.svelte
+++ src/App.svelte
@@ -11,9 +11,7 @@
 <main
   class="h-dvh flex flex-col bg-gradient-to-br from-blue-1000 via-blue-800 to-blue-500"
 >
-  <div class=" absolute h-100% lg:hidden">
-    Не доступно на мобильных устройствах
-  </div>
+  <div class="min-h-full lg: hidden">Не доступно на мобильных устройствах</div>
   <header class="p-5 flex gap-3 items-center bg-gray-800">
     <button
       class="px-4 py-1 rounded-lg cursor-pointer active:scale-95 transition-transform"
