a2da3afc92e42b147a714d04bef2723a8a20a44b
AlexandrGonin <150127341+AlexandrGonin@users.noreply.github.com>
2025-04-20 20:08:17 +0500

    123

--- src/App.svelte
+++ src/App.svelte
@@ -14,39 +14,41 @@
   <div class="min-h-full lg:hidden">
     <div>Не доступно на мобильных устройствах, используйте на ПК</div>
   </div>
-  <header class="p-5 flex gap-3 items-center bg-gray-800">
-    <button
-      class="px-4 py-1 rounded-lg cursor-pointer active:scale-95 transition-transform"
-      onclick={() => (currentView = views[0])}
-    >
-      <img src={logo} alt="LOGO" class="size-16" />
-    </button>
-    <nav class="grow flex justify-center items-center gap-1">
-      {#each views as view}
-        <button
-          class="px-4 py-1 rounded-lg cursor-pointer active:scale-80 transition-transform"
-          class:bg-black={currentView == view}
-          onclick={() => (currentView = view)}
-        >
-          {view}
-        </button>
-      {/each}
-    </nav>
-    <div class="size-16"></div>
-  </header>
+  <div class="hidden sm:block">
+    <header class="p-5 flex gap-3 items-center bg-gray-800">
+      <button
+        class="px-4 py-1 rounded-lg cursor-pointer active:scale-95 transition-transform"
+        onclick={() => (currentView = views[0])}
+      >
+        <img src={logo} alt="LOGO" class="size-16" />
+      </button>
+      <nav class="grow flex justify-center items-center gap-1">
+        {#each views as view}
+          <button
+            class="px-4 py-1 rounded-lg cursor-pointer active:scale-80 transition-transform"
+            class:bg-black={currentView == view}
+            onclick={() => (currentView = view)}
+          >
+            {view}
+          </button>
+        {/each}
+      </nav>
+      <div class="size-16"></div>
+    </header>
 
-  <!-- проверка на текущую страницу -->
-  {#if currentView == "Главная"}
-    <Menu />
-    <button
-      onclick={() => (currentView = views[1])}
-      class="my-25 text-2xl px-4 py-1 rounded-lg cursor-pointer active:scale-80 transition-transform bg-blue-950 w-75 mx-40"
-    >
-      Начать тренировку
-    </button>
-  {:else if currentView == "Играть"}
-    <Game />
-  {:else}
-    <Error404 />
-  {/if}
+    <!-- проверка на текущую страницу -->
+    {#if currentView == "Главная"}
+      <Menu />
+      <button
+        onclick={() => (currentView = views[1])}
+        class="my-25 text-2xl px-4 py-1 rounded-lg cursor-pointer active:scale-80 transition-transform bg-blue-950 w-75 mx-40"
+      >
+        Начать тренировку
+      </button>
+    {:else if currentView == "Играть"}
+      <Game />
+    {:else}
+      <Error404 />
+    {/if}
+  </div>
 </main>
